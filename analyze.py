ce2eed284797506b30ba6d2fc5d95a1874db083a
Dan McKinley <mcfunley@gmail.com>
2016-01-26 09:30:03 -0800

    using the right high before the shake

--- analyze.py
+++ analyze.py
@@ -5,9 +5,10 @@ from csv import DictReader
 import os
 import sys
 
-check_against_high_price = False
+check_against_high_price = True
 
-float_cols = ('recent_high', 'last_price', 'first_shaken_price', 'alltime_high',)
+float_cols = ('recent_high', 'last_price', 'first_shaken_price', 'alltime_high',
+              'preshake_high',)
 
 
 def parse_date(s):
@@ -19,8 +20,10 @@ def read_data():
     for d in DictReader(open('shaken-stocks.csv'), delimiter=';'):
         for c in float_cols:
             d[c] = float(d[c])
+
         d['first_shaken_at'] = parse_date(d['first_shaken_at'])
         data.append(d)
+
     return data
 
 
@@ -45,7 +48,7 @@ def log_exceptional(d, r):
     print >>sys.stderr, '%-10s %-40s %.2f' % (d['symbol'], d['name'], r)
 
 
-def categorize(recovery, market_return, high):
+def categorize(symbol, last, recovery, market_return, high):
     if recovery < -90:
         return 'wiped out'
     if recovery < 0:
@@ -57,7 +60,7 @@ def categorize(recovery, market_return, high):
         return 'beaten by market'
 
     if check_against_high_price:
-        if recovery < high:
+        if last < high:
             return 'recovered below high price'
 
     if recovery > 1000:
@@ -73,9 +76,9 @@ for d in data:
     last = d['last_price']
     recovery = (last - sp) / sp * 100
     market_return = sp500_return(d['first_shaken_at'])
-    high = d['alltime_high']
+    high = d['preshake_high']
 
-    categories[categorize(recovery, market_return, high)] += 1
+    categories[categorize(d['symbol'], last, recovery, market_return, high)] += 1
 
 
 n = len(data)
